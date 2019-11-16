c9f078b808980f1fa6f4777321fe254dd2495632
KAWASHIMA Yoshiyuki <ykawashi7@gmail.com>
2019-11-16 19:45:51 +0900

    Reverse the animation at the beginning or the end

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,13 @@ class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
     super.initState();
     controller = AnimationController(duration: const Duration(seconds: 2), vsync: this);
     animation = Tween<double>(begin: 0, end: 300).animate(controller)
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          controller.reverse();
+        } else if (status == AnimationStatus.dismissed) {
+          controller.forward();
+        }
+      })
       ..addStatusListener((state) => print('$state'));
     controller.forward();
   }
