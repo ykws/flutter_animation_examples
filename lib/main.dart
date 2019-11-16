fa91924229e09a0d16ffd15a50c21e4b74ac2eef
KAWASHIMA Yoshiyuki <ykawashi7@gmail.com>
2019-11-17 00:10:49 +0900

    Simultaneous animations

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,29 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(LogoApp());
 
+class AnimatedLogo extends AnimatedWidget {
+  static final _opacityTween = Tween<double>(begin: 0.1, end: 1);
+  static final _sizeTween = Tween<double>(begin: 0, end: 300);
+
+  AnimatedLogo({Key key, Animation<double> animation})
+    : super(key: key, listenable: animation);
+
+  Widget build(BuildContext context) {
+    final animation = listenable as Animation<double>;
+    return Center(
+      child: Opacity(
+        opacity: _opacityTween.evaluate(animation),
+        child: Container(
+          margin: EdgeInsets.symmetric(vertical: 10),
+          height: _sizeTween.evaluate(animation),
+          width: _sizeTween.evaluate(animation),
+          child: FlutterLogo(),
+        ),
+      ),
+    );
+  }
+}
+
 class LogoWidget extends StatelessWidget {
   Widget build(BuildContext context) => Container(
     margin: EdgeInsets.symmetric(vertical: 10),
@@ -42,7 +65,7 @@ class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
   void initState() {
     super.initState();
     controller = AnimationController(duration: const Duration(seconds: 2), vsync: this);
-    animation = Tween<double>(begin: 0, end: 300).animate(controller)
+    animation = CurvedAnimation(parent: controller, curve: Curves.easeIn)
       ..addStatusListener((status) {
         if (status == AnimationStatus.completed) {
           controller.reverse();
@@ -55,10 +78,7 @@ class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
   }
 
   @override
-  Widget build(BuildContext context) => GrowTransition(
-    child: LogoWidget(),
-    animation: animation,
-  );
+  Widget build(BuildContext context) => AnimatedLogo(animation: animation);
 
   @override
   void dispose() {
